e3094205850afc16324c4d762cf3885b4f765e79
wuq720 <48305620+wuq720@users.noreply.github.com>
2020-02-06 12:04:58 +0800

    decoding

--- SolutionRepresentation.py
+++ SolutionRepresentation.py
@@ -18,29 +18,24 @@ def encoding(precedence_matrix):
 def decoding(task_sequence, time_list, nb_station):
     init_cycletime, cycletime = sum(time_list) / nb_station, sum(time_list)
     while cycletime > init_cycletime:
-        print(cycletime)
-        print(init_cycletime)
         start = 0
         assignment_task = {}
         workload = []
+        potential_workload = []
         for m in range(nb_station - 1):
             end, stationTime = start, time_list[start]
             while stationTime <= init_cycletime:
                 end += 1
-                stationTime += time_list[end]
+                stationTime += time_list[task_sequence[end]]
             assignment_task[m] = task_sequence[start:end]
-            workload.append(stationTime - time_list[end])
+            workload.append(stationTime - time_list[task_sequence[end]])
             start = end
         assignment_task[m+1] = task_sequence[start:]
         workload.append(sum(time_list[_] for _ in assignment_task[m+1]))
-        potential_workload = []
         for m in range(len(workload) - 1):
             potential_task = assignment_task[m+1][0]
             potential_workload.append(workload[m] + time_list[potential_task])
         cycletime, init_cycletime = max(workload), min(potential_workload)
-        print(workload)
-        print(potential_workload)
-        print(assignment_task)
     return cycletime, assignment_task
 
 
