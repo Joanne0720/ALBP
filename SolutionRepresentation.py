b33f456530b74971be5fcbd696a7dc7104676a8d
wuq720 <48305620+wuq720@users.noreply.github.com>
2020-02-07 13:00:23 +0800

    decoding

--- SolutionRepresentation.py
+++ SolutionRepresentation.py
@@ -1,42 +1,44 @@
-import numpy as np
 import random
+import copy
 
 
 def encoding(precedence_matrix):
-    matrix = np.array(precedence_matrix)
-    result = []
-    for _ in range(len(matrix)):
+    taskSequence = []
+    n = len(precedence_matrix)
+    M = copy.deepcopy(precedence_matrix)
+    for _ in range(n):
         taskSet = []
-        for i in range(len(matrix)):
-            if all(matrix[i, :]) == 0 and i not in result: taskSet.append(i)
+        for j in range(n):
+            if sum(_[j] for _ in M) == 0 and j not in taskSequence: taskSet.append(j)
         task = random.choice(taskSet)
-        result.append(task)
-        for n in range(len(matrix)): matrix[n, task] = 0
-    return result
+        taskSequence.append(task)
+        for j in range(n): M[task][j] = 0
+    return taskSequence
 
 
 def decoding(task_sequence, time_list, nb_station):
     init_cycletime, cycletime = sum(time_list) / nb_station, sum(time_list)
     while cycletime > init_cycletime:
-        start = 0
-        assignment_task = {}
-        workload = []
-        potential_workload = []
-        for m in range(nb_station - 1):
-            end, stationTime = start, time_list[start]
-            while stationTime <= init_cycletime:
-                end += 1
-                stationTime += time_list[task_sequence[end]]
-            assignment_task[m] = task_sequence[start:end]
-            workload.append(stationTime - time_list[task_sequence[end]])
-            start = end
-        assignment_task[m+1] = task_sequence[start:]
-        workload.append(sum(time_list[_] for _ in assignment_task[m+1]))
+        task_unassigned = task_sequence[:]
+        workload, potential_workload = [], []
+        station_num = 0
+        task_to_station = []
+        while station_num < nb_station:
+            currTime, currTasks = 0, []
+            while task_unassigned and currTime + time_list[task_unassigned[0]] <= init_cycletime:
+                currTime += time_list[task_unassigned[0]]
+                currTasks.append(task_unassigned.pop(0))
+            workload.append(currTime)
+            task_to_station.append(currTasks)
+            station_num += 1
+        if task_unassigned:
+            workload[station_num-1] += sum(time_list[_] for _ in task_unassigned)
+            task_to_station[station_num-1] += task_unassigned
         for m in range(len(workload) - 1):
-            potential_task = assignment_task[m+1][0]
+            potential_task = task_to_station[m+1][0]
             potential_workload.append(workload[m] + time_list[potential_task])
         cycletime, init_cycletime = max(workload), min(potential_workload)
-    return cycletime, assignment_task
+    return cycletime, task_to_station
 
 
 
