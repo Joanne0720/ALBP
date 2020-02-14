9fc47c60276160066da168a1dd01de654b1d07dc
wuq720 <48305620+wuq720@users.noreply.github.com>
2020-02-14 17:44:50 +0800

    CP model for ALBP

--- CPModel_ALBP.py
+++ CPModel_ALBP.py
@@ -1,15 +1,18 @@
-from CplexModel_ALBP import ALBP
 from docplex.cp.model import CpoModel
 from docplex.cp.config import context
+
 context.solver.agent = 'local'
 context.solver.local.execfile = '/Applications/CPLEX_Studio_Community129/cpoptimizer/bin/x86-64_osx/cpoptimizer'
 
-"""Initialize the problem data"""
+""" Initialize the problem data """
+# operating time of task i
+TaskTime = [81, 109, 65, 51, 92, 77, 51, 50, 43, 45, 76]
 
-TaskTimeMin = [81, 109, 65, 51, 92, 77, 51, 50, 43, 45, 76]  # operating time of task i
-TaskTimeMax = [181, 209, 165, 151, 192, 177, 151, 150, 143, 145, 176]
-nbStations = 5  # number of workstations
-PrecedenceTasks = [  # immediate precedence tasks of task i
+# number of workstations
+nbStations = 5
+
+# immediate precedence tasks of task i
+PrecedenceTasks = [
     [],
     [1],
     [1],
@@ -20,58 +23,42 @@ PrecedenceTasks = [  # immediate precedence tasks of task i
     [1, 2, 6],
     [1, 3, 4, 5, 7],
     [1, 2, 6, 8],
-    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-]
+    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]]
 
-"""Prepare the data for modeling"""
+""" Prepare the data for modeling """
+# tasks set
+TASKS = range(len(TaskTime))
+# workstations set
+WORKSTATIONS = range(nbStations)
 
-TASKS = range(len(TaskTimeMin))  # tasks set
-WORKSTATIONS = range(nbStations)  # workstations set
-# SCENARIOS = [0]  # scenarios set
-
-
-"""Build the model"""
-
-'''Create CPO model'''
+""" Build the model """
+# Create CPO model
 myModel = CpoModel()
-'''Assign tasks to workstation'''
+
+# Assign tasks to workstation
 WhichWorkstation = myModel.integer_var_list(len(TASKS), 0, nbStations - 1)
-'''Cycle time'''
-CycleTime = myModel.integer_var(max(TaskTimeMin), sum(TaskTimeMax))
 
-# Regret = myModel.integer_var()
-# TaskTimeScenario = myModel.integer_var_list(TASKS)
-# OptimalCycleTime = myModel.integer_var(ub=sum(TaskTimeMax))
+# Cycle time
+CycleTime = myModel.integer_var(max(TaskTime) / nbStations, sum(TaskTime))
 
-'''Create constraints'''
-# s = 0
-# for i in TASKS:
-#     myModel.add_if_then(WhichWorkstation[i, 0] == 1, TaskTimeScenario[i] == TaskTimeMax[i])
-#     myModel.add_if_then(WhichWorkstation[i, 0] == 0, TaskTimeScenario[i] == TaskTimeMin[i])
-# temp = []
-# for _ in TASKS:
-#     temp.append(TaskTimeScenario[s, _])
-# OptimalCycleTime[s] = ALBP(temp, nbStations, PrecedenceTasks)
-# myModel.add(CycleTime == sum(TaskTimeMax[i] * WhichWorkstation[i, 1] for i in TASKS))
-# myModel.add(Regret >= CycleTime)
-
-'''ALBP constraints'''
 # Add station load constraints
 for k in WORKSTATIONS:
     load = 0
     for i in TASKS:
         flag = WhichWorkstation[i] == k
-        load += TaskTimeMin[i]*flag
+        load += TaskTime[i] * flag
     myModel.add(load <= CycleTime)
 
 # Add precedence constraints
 for i in TASKS:
     for j in PrecedenceTasks[i]:
         myModel.add(WhichWorkstation[j] <= WhichWorkstation[i])
-''''''
-'''Create model objective'''
+
+# Create model objective
 myModel.add(myModel.minimize(CycleTime))
 
-
+""" Solve model """
 r = myModel.solve(FailLimit=100000, TimeLimit=10)
+
+""" Print solution """
 r.print_solution()
